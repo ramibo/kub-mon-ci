c62f7e888a2b7268eb1d76774e6e5e5ffab20052
rammatzkvosky <41573396+rammatzkvosky@users.noreply.github.com>
2023-01-15 02:05:39 +0200

    new commit

--- src/server.py
+++ src/server.py
@@ -3,14 +3,17 @@ import prometheus_client
 from prometheus_client.core import CollectorRegistry
 from prometheus_client import Summary, Counter, Histogram, Gauge
 import time
+import psutil
 
 app = Flask(__name__)
+CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
 
 _INF = float("inf")
 
 graphs = {}
 graphs['c'] = Counter('python_request_operations_total', 'The total number of processed requests')
 graphs['h'] = Histogram('python_request_duration_seconds', 'Histogram for the duration in seconds.', buckets=(1, 2, 5, 6, 10, _INF))
+graphs['m'] = Gauge('system_usage','Hold current system resource usage',['resource_type'])
 
 @app.route("/")
 def hello():
@@ -20,6 +23,9 @@ def hello():
     time.sleep(0.600)
     end = time.time()
     graphs['h'].observe(end - start)
+    graphs['m'].labels('CPU').set(psutil.cpu_percent())
+    graphs['m'].labels('Memory').set(psutil.virtual_memory()[2])
+
     return "Hello World!"
 
 @app.route("/metrics")
@@ -30,4 +36,4 @@ def requests_count():
     return Response(res, mimetype="text/plain")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=5001)
